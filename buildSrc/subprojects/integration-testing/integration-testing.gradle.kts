d713a6d2da86d8e523cd7aca20537d609e376b3f
Jendrik Johannes <jendrik@gradle.com>
2020-07-01 17:45:52 +0200

    Use included builds instead of buildSrc to organize build logic

--- buildSrc/subprojects/integration-testing/integration-testing.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    implementation(project(":basics"))
-    implementation(project(":cleanup"))
-    implementation(project(":dependencyModules"))
-    implementation(project(":packaging"))
-    implementation(project(":moduleIdentity"))
-
-    testImplementation("junit:junit")
-}
